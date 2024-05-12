f2c7b211493e7db295c6bfc41afc0cd5df02211f
nebulajazz <122885800+nebulajazz@users.noreply.github.com>
2024-05-13 00:13:22 +0530

    Create kmp.c

---
+++ kmp.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <string.h>
+
+void prefixSuffixArray(char pat[], int M, int pps[]) {
+    int length = 0;
+    pps[0] = 0;
+    int i = 1;
+    while (i < M) {
+        if (pat[i] == pat[length]) {
+            length++;
+            pps[i] = length;
+            i++;
+        }
+        else {
+            if (length != 0)
+                length = pps[length - 1];
+            else {
+                pps[i] = 0;
+                i++;
+            }
+        }
+    }
+}
+
+void KMPAlgorithm(char text[], char pattern[]) {
+    int M = strlen(pattern);
+    int N = strlen(text);
+    int pps[M];
+    prefixSuffixArray(pattern, M, pps);
+    int i = 0;
+    int j = 0;
+    while (i < N) {
+        if (pattern[j] == text[i]) {
+            j++;
+            i++;
+        }
+        if (j == M) {
+            printf("Found pattern at index %d\n", i - j);
+            return;
+        }
+        else if (i < N && pattern[j] != text[i]) {
+            if (j != 0)
+                j = pps[j - 1];
+            else
+                i = i + 1;
+        }
+    }
+    printf("Pattern not found\n");
+}
+
+int main() {
+    char text[100], pattern[100];
+    
+    printf("Enter the text: ");
+    scanf("%s", text);
+
+    printf("Enter the pattern: ");
+    scanf("%s", pattern);
+
+    KMPAlgorithm(text, pattern);
+    return 0;
+}
